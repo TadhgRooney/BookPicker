9009db266793b7e8a3811a1c29c0768afae14b87
TadhgRooney <tadhgrooney17@gmail.com>
2024-01-27 12:07:46 +0000

    Fixed Layout

--- BookPicker.java
+++ BookPicker.java
@@ -1,25 +1,41 @@
-import java.util.Scanner;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
+import java.util.Scanner;
 
-public class BookPicker
-{
-  public static void main(String[] args) {
-    Random rand = new Random();
-    int book;
-    String reply;
-    Scanner scan = new Scanner(System.in);
+public class BookPicker{
 
-    do{
-      System.out.println("Press A to spin ");
-      reply = scan.nextLine();
-      if(reply.equalsIgnoreCase("A"))
-      {
-        book = rand.nextInt(272) +1;
-        System.out.println(book);
+  Queue<String> bookQueue = new LinkedList<String>();
 
-      }
 
-    }while(reply.equalsIgnoreCase("A"));
-  
-  }
+
+public BookPicker(){
+  randomBook();
+}
+
+public void randomBook(){
+
+        Random rand = new Random();
+        int book;
+        String reply;
+        Scanner scan = new Scanner(System.in);
+        do
+        {
+            System.out.print("Press A to spin");
+            reply=scan.nextLine();
+            if(reply.equalsIgnoreCase("A"))
+            {
+                book = rand.nextInt(272)+ 1;
+                System.out.println(book);
+            }
+        }while(reply.equalsIgnoreCase("A"));
+    }
+
+
+
+public static void main(String[] args) {
+   new BookPicker();
+    
+}
+
 }
